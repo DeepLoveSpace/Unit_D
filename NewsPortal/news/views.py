b21bae5fdc3c5df7599a58c6613d79ee44658d0d
DeepLove <titan182@yandex.ru>
2024-11-07 01:59:08 +0300

    Задание D_3

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,1 +1,16 @@
 from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-post_time'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
