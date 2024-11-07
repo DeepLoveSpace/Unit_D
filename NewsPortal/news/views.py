22155aabd90747fbb2a98cb3663014405b70b254
DeepLove <titan182@yandex.ru>
2024-11-07 05:13:29 +0300

    Задание D_4

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-
 from django.views.generic import ListView, DetailView
+
+from .filters import PostFilter
 from .models import Post
 
 
@@ -9,6 +9,17 @@ class PostsList(ListView):
     ordering = '-post_time'
     template_name = 'news.html'
     context_object_name = 'news'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
 
 class PostDetail(DetailView):
     model = Post
